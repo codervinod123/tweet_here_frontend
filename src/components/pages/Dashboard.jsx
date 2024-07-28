d08d543aae63b58de4f4bd535f2cfbdf0b251ab5
vinod kumar <88377243+codervinod123@users.noreply.github.com>
2024-07-28 15:17:29 +0530

    tweets fetched from database

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-
-
-
-
+import React, { useEffect,useState } from 'react'
 import IMG from "../../../src/assets/vinod.jpg"
 import Navigation from '../NavigationMenu';
 import Post from '../Post';
 import Tweet from '../Tweet';
 
+import axios from "axios";
 
 
 const Dashboard = () => {
+
+  const [tweet,setTweet]=useState([]);
+
+  useEffect(()=>{
+      getTweets();
+  },[])
+
+  const getTweets=async()=>{
+         const res=await axios.get("http://localhost:3001/api/v1/tweet");
+         setTweet(res.data.data);
+  }
+
   return (
     <div className='w-[100%] h-[100vh] bg-black grid grid-cols-5 text-white'>
         
@@ -25,11 +34,7 @@ const Dashboard = () => {
               </div>
 
                <Post/>
-               <Tweet/>
-               <Tweet/>
-               <Tweet/>
-               <Tweet/>
-               <Tweet/>
+               <Tweet tweet={tweet}/>
         </div>
 
         <div className='text-white border-l'>kds</div>
